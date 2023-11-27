8cbbc7a0612a2963dac16c85e247af3719778bb7
Yuan <yuanji.yj@gmail.com>
2023-11-26 22:11:30 -0500

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Page() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">Yuan Ji</h1>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my portfolio</h1>
       <p className="prose prose-neutral dark:prose-invert">
-        This is my new portfolio.
+        This is your new portfolio.
       </p>
     </section>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 import typography from '@tailwindcss/typography';
 
 export default {
-  content: ['./app/**/*.{ts,tsx}', './content/**/*.mdx'],
+  content: ['./app/**/*.{ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
