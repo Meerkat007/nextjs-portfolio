3b65c7bf90a4184560a30b0224835e79d500e64d
Yuan <yuanji.yj@gmail.com>
2023-11-27 16:59:43 -0500

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my portfolio</h1>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">Yuan Ji</h1>
       <p className="prose prose-neutral dark:prose-invert">
         This is your new portfolio.
       </p>
